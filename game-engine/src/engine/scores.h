383124aac25cefddb248ddd430d704e49e5e2ad9
mondher <mondhermsp@gmail.com>
2023-12-07 22:25:17 +0100

    structure score

---
+++ game-engine/src/engine/scores.h
@@ -0,0 +1,9 @@
+// Structure score nom, score
+
+// WriteLocalScore(nom, score) -> Crée un fichier dans le dossier du jeu avec tous les scores locaux
+
+// GetLocalScore() -> Retourne Tableau de structure score contenant les score locaux
+
+// SendOnlineScore(nom, score) -> requete post a la base de donnee
+
+// GetOnlineScore() -> Retourne Tableau de structure score contenant les score de la base
